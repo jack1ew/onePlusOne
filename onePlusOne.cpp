c65897c0a56c4605bb6dffc544313b1020c6bea6
jack1ew <jackiew@email.sc.edu>
2022-12-29 21:20:43 -0500

    new file

---
+++ onePlusOne.cpp
@@ -0,0 +1,11 @@
+// Jackie Wang
+// One plus one cod that is supposed to be impelemented on fpga
+int main() {
+  int i = 0;
+  while(true) {
+    i = 1 + 1;
+    std::cout << i << endl;
+  }
+
+  return 0;
+}
